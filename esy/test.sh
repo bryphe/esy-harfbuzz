cba93c4381c7961d5543eb6686e3893b74486cde
Bryan Phelps <bryphe@outlook.com>
2018-10-10 16:33:26 -0700

    Update build script

--- esy/test.sh
+++ esy/test.sh
@@ -5,10 +5,8 @@ INCLUDE=$cur__install/include/harfbuzz
 
 if which x86_64-w64-mingw32-gcc; then
     CC=x86_64-w64-mingw32-gcc
-    CCLIB=
 else
     CC=gcc
-    CCLIB="-lbz2 -lpng -lz"
 fi
 
 echo "Using compiler: $CC"
@@ -20,5 +18,5 @@ ls -a $cur__lib
 
 cp $cur__bin/*.dll .
 
-$CC ./esy/test.c -o ./test -I$INCLUDE -L$cur__lib -lharfbuzz -L/usr/lib -L/usr/local/lib $CCLIB
+$CC ./test.c -o ./test -I$INCLUDE -L$cur__lib -lharfbuzz
 ./test

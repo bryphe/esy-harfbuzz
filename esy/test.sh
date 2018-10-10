3d04d7cd3e7c61fc5e9f39f412547b14d13875da
Bryan Phelps <bryphe@outlook.com>
2018-10-10 16:29:32 -0700

    Test program

--- esy/test.sh
+++ esy/test.sh
@@ -1,1 +1,24 @@
-echo TODO: Test!
+cd esy
+pwd
+
+INCLUDE=$cur__install/include/harfbuzz
+
+if which x86_64-w64-mingw32-gcc; then
+    CC=x86_64-w64-mingw32-gcc
+    CCLIB=
+else
+    CC=gcc
+    CCLIB="-lbz2 -lpng -lz"
+fi
+
+echo "Using compiler: $CC"
+
+echo "include..."
+ls -a $INCLUDE
+echo "lib.."
+ls -a $cur__lib
+
+cp $cur__bin/*.dll .
+
+$CC ./esy/test.c -o ./test -I$INCLUDE -L$cur__lib -lharfbuzz -L/usr/lib -L/usr/local/lib $CCLIB
+./test

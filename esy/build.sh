3fa5abfb2fb5f7111dd723a9b3bec42d86bb49e3
Bryan Phelps <bryphe@outlook.com>
2018-10-11 09:27:56 -0700

    Update build / test files

--- esy/build.sh
+++ esy/build.sh
@@ -1,10 +1,12 @@
 cd _build
 
-# OS_WIN32 invokes a python def file generator for MVSC linking,
-# which we don't need (it's implied by host - mingw)
+# Automake gets brought in unnecessarily for the release package - 
+# this rewinds time for dependencies so automake doesn't get triggered
 touch -t 200101010000 aclocal.m4
 # touch src/Makefile.in
 
+# OS_WIN32 invokes a python def file generator for MVSC linking,
+# which we don't need (it's implied by host - mingw)
 touch src/harfbuzz.def
 touch src/harfbuzz-subset.def
 touch src/harfbuzz-icu.def
--- esy/test.sh
+++ esy/test.sh
@@ -1,17 +1,22 @@
 
 INCLUDE=$cur__install/include/harfbuzz
 
+ROOTDIR=$(pwd)
+
 if which x86_64-w64-mingw32-gcc; then
     CC=x86_64-w64-mingw32-gcc
     # Copy runtime mingw files
-    DEST=$(pwd)
-    cp /usr/x86_64-w64-mingw32/sys-root/mingw/bin/*.dll .
+    BUILDDIR=$(pwd)/esy
+    cp /usr/x86_64-w64-mingw32/sys-root/mingw/bin/*.dll $BUILDDIR/.
 else
     CC=gcc
-    DEST=_build
+    BUILDDIR=_build
 fi
 
-cd $DEST
+echo "Using build directory: $BUILDDIR"
+echo "Root directory: $ROOTDIR"
+
+cd $BUILDDIR
 pwd
 
 echo "Using compiler: $CC"
@@ -22,5 +27,5 @@ echo "Bin Path: $cur__bin"
 
 cp $cur__bin/*.dll .
 
-$CC ./../esy/test.c -o ./test.exe -I$INCLUDE -L$cur__lib -lharfbuzz
-./test.exe ./../esy/Roboto-Regular.ttf "test=>text"
+$CC $ROOTDIR/esy/test.c -o $BUILDDIR/test.exe -I$INCLUDE -L$cur__lib -lharfbuzz
+$BUILDDIR/test.exe $ROOTDIR/esy/Roboto-Regular.ttf "test=>text"

5887904f6733323a20f398730b6ac5ad100fd490
Bryan Phelps <bryphe@outlook.com>
2018-10-11 09:18:52 -0700

    Update permissions / test directory

--- esy/test.sh
+++ esy/test.sh
@@ -1,15 +1,18 @@
-cd _build
-pwd
 
 INCLUDE=$cur__install/include/harfbuzz
 
 if which x86_64-w64-mingw32-gcc; then
     CC=x86_64-w64-mingw32-gcc
     # Copy runtime mingw files
+    DEST=$(pwd)
     cp /usr/x86_64-w64-mingw32/sys-root/mingw/bin/*.dll .
 else
     CC=gcc
+    DEST=_build
 fi
+
+cd $DEST
+pwd
 
 echo "Using compiler: $CC"
 
@@ -19,5 +22,5 @@ echo "Bin Path: $cur__bin"
 
 cp $cur__bin/*.dll .
 
-$CC ./../esy/test.c -o ./test.exe $FLAGS -I$INCLUDE -L$cur__lib -lharfbuzz
-./test.exe ./../esy/Roboto-Regular.ttf test=>text
+$CC ./../esy/test.c -o ./test.exe -I$INCLUDE -L$cur__lib -lharfbuzz
+./test.exe ./../esy/Roboto-Regular.ttf "test=>text"

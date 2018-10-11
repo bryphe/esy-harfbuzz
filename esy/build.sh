fcfb506ef3a9d2fe56bc730ca40334a2b73fe518
Bryan Phelps <bryphe@outlook.com>
2018-10-11 09:03:39 -0700

    Remove touching files

--- esy/build.sh
+++ esy/build.sh
@@ -2,7 +2,8 @@ cd _build
 
 # OS_WIN32 invokes a python def file generator for MVSC linking,
 # which we don't need (it's implied by host - mingw)
-touch aclocal.m4
+# touch aclocal.m4
+# touch src/Makefile.in
 
 touch src/harfbuzz.def
 touch src/harfbuzz-subset.def

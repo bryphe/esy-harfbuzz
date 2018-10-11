c3b75c57d6ee582a2c456673b82f7b99a56d0319
Bryan Phelps <bryphe@outlook.com>
2018-10-11 08:50:52 -0700

    Touch proper file

--- esy/build.sh
+++ esy/build.sh
@@ -2,7 +2,7 @@ cd _build
 
 # OS_WIN32 invokes a python def file generator for MVSC linking,
 # which we don't need (it's implied by host - mingw)
-touch src/aclocal.m4
+touch aclocal.m4
 
 touch src/harfbuzz.def
 touch src/harfbuzz-subset.def

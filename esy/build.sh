f5d9e0cd0b208da68c158de7d9408b141ceaaf66
Bryan Phelps <bryphe@outlook.com>
2018-10-11 09:25:08 -0700

    Add touch command

--- esy/build.sh
+++ esy/build.sh
@@ -2,7 +2,7 @@ cd _build
 
 # OS_WIN32 invokes a python def file generator for MVSC linking,
 # which we don't need (it's implied by host - mingw)
-touch -t 200905000000 aclocal.m4
+touch -t 200101010000 aclocal.m4
 # touch src/Makefile.in
 
 touch src/harfbuzz.def
